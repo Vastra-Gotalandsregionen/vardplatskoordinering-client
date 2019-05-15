e6139cc4e4dfd4c8387d12049b670fd2c556adab
mrg3kko <erik.andersson@monator.com>
2019-05-15 11:37:15 +0200

    Started on vpk-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './view/home/home.component';
-import { HeaderComponent } from './elements/header/header.component';
-import { CoordinationComponent } from './view/coordination/coordination.component';
-import { StyleguideComponent } from './view/styleguide/styleguide.component';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatTableModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
+// Elements
+import { HeaderComponent } from './elements/header/header.component';
+import { VpkCardComponent } from './elements/vpk-card/vpk-card.component';
+
+// Views
+import { HomeComponent } from './view/home/home.component';
+import { CoordinationComponent } from './view/coordination/coordination.component';
+import { StyleguideComponent } from './view/styleguide/styleguide.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    // Elements
     HeaderComponent,
+    VpkCardComponent,
+    //Views
+    HomeComponent,
     CoordinationComponent,
     StyleguideComponent
   ],
---
+++ src/app/elements/vpk-card/vpk-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vpk-card',
+  templateUrl: './vpk-card.component.html',
+  styleUrls: ['./vpk-card.component.scss']
+})
+export class VpkCardComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() icon: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
