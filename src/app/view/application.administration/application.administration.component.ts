88d43fbbadb0afbba0c128a0e3daf1cf95caea6e
mrg3kko <erik.andersson@monator.com>
2019-05-22 15:42:08 +0200

    Updated application.administration to use styleguide components

--- src/app/view/application.administration/application.administration.component.ts
+++ src/app/view/application.administration/application.administration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavItem } from '../../domain/NavItem';
 
 @Component({
   selector: 'app-administration',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationAdministrationComponent implements OnInit {
 
+  navItems: NavItem[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.navItems = this.getNavItems();
   }
+
+  getNavItems(): NavItem[] {
+    let navItems: NavItem[]  = [];
+
+    let navItem1 = new NavItem('Förvaltningar', 'Välj', '/managements', '', '');
+    let navItem2 = new NavItem('Områden', 'Välj', '/areas', '', '');
+    let navItem3 = new NavItem('Avdelningar', 'Välj', '/units', '', '');
+    let navItem4 = new NavItem('Användare', 'Välj', '/user-admin', '', '');
+    let navItem5 = new NavItem('Definitioner', 'Välj', '/definition', '', '');
+
+    navItems.push(navItem1);
+    navItems.push(navItem2);
+    navItems.push(navItem3);
+    navItems.push(navItem4);
+    navItems.push(navItem5);
+
+    return navItems;
+  }
+
 
 }
