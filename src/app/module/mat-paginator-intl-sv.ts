7f0a8adbc1590bd1aad2a5cc3005f99b89433205
mrg3kko <erik.andersson@monator.com>
2019-06-04 10:14:22 +0200

    Added getRangeLabel to MatPaginatorIntlSv

--- src/app/module/mat-paginator-intl-sv.ts
+++ src/app/module/mat-paginator-intl-sv.ts
@@ -1,4 +1,5 @@
 import {MatPaginatorIntl} from '@angular/material';
+import { getFactoryOf } from '@angular/core/src/render3';
 
 export class MatPaginatorIntlSv extends MatPaginatorIntl {
   itemsPerPageLabel = 'Poster per sida';
@@ -6,4 +7,22 @@ export class MatPaginatorIntlSv extends MatPaginatorIntl {
   previousPageLabel = 'Föregående';
   firstPageLabel = 'Första';
   lastPageLabel = 'Sista';
+  
+
+
+  getRangeLabel = (page: number, pageSize: number, length: number):string => {
+    if (length === 0 || pageSize === 0) {
+      return `0 av ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    // If the start index exceeds the list length, do not try and fix the end index to the end.
+    const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} av ${length}`;
+  };
+
+  
+
 }
