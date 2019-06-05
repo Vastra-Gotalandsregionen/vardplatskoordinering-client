200046315dc3c895206972698b87c01b5b3dea56
mrg3kko <erik.andersson@monator.com>
2019-06-05 13:39:41 +0200

    Fixed duplicate imports due to accepting both changes in previous commit

--- src/app/view/vpl-landing/vpl-areas/vpl/vpl.component.ts
+++ src/app/view/vpl-landing/vpl-areas/vpl/vpl.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Management } from '../../domain/Management';
-import { AuthService } from '../../service/auth.service';
-import { combineLatest} from 'rxjs';
+import { AuthService } from '../../../../service/auth.service';
 import { Management } from '../../../../domain/Management';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
