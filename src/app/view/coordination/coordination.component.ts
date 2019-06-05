fb3806f4e4f5f82119a6103e5b8cfa3ef0ebbf0a
mrg3kko <erik.andersson@monator.com>
2019-06-05 09:28:29 +0200

    Fixed locale from se-SE to sv-SE in coordination component dateSelected

--- src/app/view/coordination/coordination.component.ts
+++ src/app/view/coordination/coordination.component.ts
@@ -177,7 +177,7 @@ export class CoordinationComponent implements OnInit {
 
   dateSelected(thing, event: any) {
     const d: Date = event.value;
-    const dateString = d.toLocaleDateString('se-SE');
+    const dateString = d.toLocaleDateString('sv-SE');
     const dateStringIEWorkaround = dateString.replace(/\u200E/g, '');
     this.router.navigate([], {queryParams: {date: dateStringIEWorkaround}});
   }
