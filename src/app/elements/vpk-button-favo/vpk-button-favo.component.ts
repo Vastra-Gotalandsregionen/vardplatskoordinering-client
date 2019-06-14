b451a559d3bc7b63faf8eca141c3d67fc3cda9a8
claeslundahl <uberhub1>
2019-06-14 15:38:52 +0200

    Added support for star marking of pages.

--- src/app/elements/vpk-button-favo/vpk-button-favo.component.ts
+++ src/app/elements/vpk-button-favo/vpk-button-favo.component.ts
@@ -1,9 +1,9 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
-import { GlobalStateService } from '../../service/global-state.service';
-import { StateService } from '../../service/state.service';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../../service/auth.service';
-import { FavoriteLink } from '../../domain/FavoriteLink';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {GlobalStateService} from '../../service/global-state.service';
+import {StateService} from '../../service/state.service';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../service/auth.service';
+import {FavoriteLink} from '../../domain/FavoriteLink';
 
 @Component({
   selector: 'vpk-button-favo',
@@ -20,6 +20,8 @@ export class VpkButtonFavoComponent implements OnInit {
   @Input() name: string;
 
   public isFavorite: boolean = false;
+
+  public model: FavoriteLink = null;
 
   @HostBinding('class')
   get classes(): string {
@@ -39,9 +41,17 @@ export class VpkButtonFavoComponent implements OnInit {
   constructor(private global: GlobalStateService, private stateService: StateService, private http: HttpClient, private authService: AuthService) {
     global.getManagementId().subscribe(n => console.log('managementId', n));
     console.log(stateService);
-    http.get('api/favorite-link/username/' + authService.getLoggedInUserId() + '?url="' + encodeURIComponent(location.pathname.replace('/', '_')) + '"').subscribe(r => {
+    const fetchUrl = 'api/favorite-link/username/' + authService.getLoggedInUserId() + '?url="' + encodeURIComponent(location.pathname.replace('/', '_')) + '"';
+    console.log(fetchUrl);
+    http.get(fetchUrl).subscribe((r: FavoriteLink[]) => {
       console.log('Result for favo', r);
-      this.isFavorite = r != null;
+      this.isFavorite = r.length == 1;
+      if (r.length == 1) {
+        this.model = r[0];
+      } else {
+        this.model = null;
+        this.isFavorite = false;
+      }
     });
   }
 
@@ -50,18 +60,26 @@ export class VpkButtonFavoComponent implements OnInit {
   }
 
   public toggleFavoriteForCurrentPage() {
-    const favo = new FavoriteLink();
-    favo.name = this.name;
-    var info = location.pathname.split('/')[1];
-    info = info.charAt(0).toUpperCase() + info.substring(1);
-    favo.info = info;
-    favo.url = location.pathname;
+    if (!this.isFavorite) {
+      const favo = new FavoriteLink();
+      favo.name = document.getElementsByClassName('vpk-title-row-heading')[0].textContent.split(',')[0];
 
-    console.log('toggleFavoriteForCurrentPage', favo);
-    this.http.put('/api/favorite-link/user/' + this.authService.getLoggedInUserId(), favo).subscribe((result: FavoriteLink) => {
-      console.log('Saved as', result);
-      this.isFavorite = true;
-    });
+      favo.info = this.name;
+      favo.url = location.pathname;
+
+      console.log('toggleFavoriteForCurrentPage', favo);
+      this.http.put('/api/favorite-link/user/' + this.authService.getLoggedInUserId(), favo).subscribe((result: FavoriteLink) => {
+        console.log('Saved as', result);
+        this.isFavorite = true;
+        this.model = result;
+      });
+    } else {
+      this.http.delete('/api/favorite-link/' + this.model.id).subscribe(r => {
+        console.log('Removed ', r);
+        this.isFavorite = false;
+        this.model = null;
+      });
+    }
   }
 
   isLoggedIn() {
