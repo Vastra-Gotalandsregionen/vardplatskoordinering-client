226296082ec490293429467fd3a7a740840fad15
mrg3kko <erik.andersson@monator.com>
2019-07-03 14:42:03 +0200

    Added responsive behaviour to VPL table and degree of impact table

--- src/app/elements/view-only-impact-dialog/view-only-impact-dialog.component.ts
+++ src/app/elements/view-only-impact-dialog/view-only-impact-dialog.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DegreeOfImpact } from '../../domain/DegreeOfImpact';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-view-only-impact-dialog',
   templateUrl: './view-only-impact-dialog.component.html',
-  styleUrls: ['./view-only-impact-dialog.component.scss']
+  styleUrls: ['./view-only-impact-dialog.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+    trigger('togglerExpand', [
+      state('collapsed', style({transform: 'rotate(0)'})),
+      state('expanded', style({transform: 'rotate(180deg)'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class ViewOnlyImpactDialogComponent implements OnInit {
 
   displayedColumns = ['degree', 'impact', 'details'];
   isLoading = true;
   impactList: DegreeOfImpact[] = [];
+  expandedElement: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
+                  
+      this.displayedColumns = result.matches ? 
+        ['toggleExpand', 'degree', 'impact'] : 
+        ['degree', 'impact', 'details'];
+    });
+  }
 
   ngOnInit() {
     this.http.get<DegreeOfImpact[]>('/api/degree-of-impact').subscribe(impactList => {
--- src/app/elements/vpl-table/vpl-table.component.ts
+++ src/app/elements/vpl-table/vpl-table.component.ts
@@ -2,13 +2,27 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { VplReg } from '../../domain/vpl-reg';
 import { AuthService } from '../../service/auth.service';
 import { MatDialog } from '@angular/material';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { EditVplRegDialogComponent } from '../edit-vpl-reg-dialog/edit-vpl-reg-dialog.component';
 import { CalculateUtil } from '../../util/calculate-util';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-vpl-table',
   templateUrl: './vpl-table.component.html',
-  styleUrls: ['./vpl-table.component.scss']
+  styleUrls: ['./vpl-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+    trigger('togglerExpand', [
+      state('collapsed', style({transform: 'rotate(0)'})),
+      state('expanded', style({transform: 'rotate(180deg)'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class VplTableComponent implements OnInit {
 
@@ -18,10 +32,21 @@ export class VplTableComponent implements OnInit {
   regDisplayedColumns = ['avd', 'regtid', 'max', 'inneliggande', 'hem', 'hemp', 'planIn', 'medFardigbehandlade', 'ob', 'prognosis',
     'actions'];
 
+  expandedElement: any;
+
   today = new Date().toISOString().slice(0, 10);
 
   constructor(private authService: AuthService,
-              public dialog: MatDialog) { }
+              public dialog: MatDialog,
+              private breakpointObserver: BreakpointObserver) {
+                breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
+                  
+                  this.regDisplayedColumns = result.matches ? 
+                    ['toggleExpand', 'avd', 'prognosis','actions'] : 
+                    ['avd', 'regtid', 'max', 'inneliggande', 'hem', 'hemp', 'planIn', 'medFardigbehandlade', 'ob', 'prognosis', 'actions'];
+                });
+                
+  }
 
   ngOnInit() {
   }
