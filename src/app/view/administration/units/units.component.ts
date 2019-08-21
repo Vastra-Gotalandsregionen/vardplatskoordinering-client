e290e65b9b282dc3254d891e6e6945e744d758bf
nadei1 <nader.einanlo@vgregion.se>
2019-08-21 13:02:33 +0200

    tabellen för avdelningar i administration sidan listas när den har hämtats.

--- src/app/view/administration/units/units.component.ts
+++ src/app/view/administration/units/units.component.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { VplUnit } from '../../../domain/vpl-unit';
 import { Administration } from '../../../domain/Administration';
 import { concatAll, flatMap, map, toArray } from 'rxjs/operators';
-import { forkJoin, of } from 'rxjs';
+import {BehaviorSubject, forkJoin, of} from 'rxjs';
 import { Management } from '../../../domain/Management';
 
 @Component({
@@ -16,6 +16,7 @@ import { Management } from '../../../domain/Management';
 export class UnitsComponent implements OnInit {
 
   resourceUrl = '/api/vpl-unit';
+  isLoading = true;
 
   fieldsConfigs: FieldConfig[] = [];
 
@@ -26,28 +27,33 @@ export class UnitsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dataSource.load();
+    const userLoadingSubject = new BehaviorSubject(null);
+    const subscription = this.dataSource.load();
 
-    this.updateFieldsConfigByFetchingWithADbJoin();
+    subscription.add(teardown => {
+      userLoadingSubject.next(1);
+      userLoadingSubject.complete();
+    });
+    userLoadingSubject.finally(() => {
+      this.isLoading = false;
+    }).subscribe(() => {
+      this.http.get<Administration[]>('/api/administration')
+        .subscribe((administrations: Administration[]) => {
+          const administrationOptions = administrations.map(a => ({label: a.verks, value: a.id}));
 
+          this.fieldsConfigs = [
+            FieldConfig.from('avd', 'Namn', 'input', null, false, true),
+            FieldConfig.from('sjh', 'Sjukhus', 'select', [{label: 'NÄL', value: 'NÄL'}, {label: 'US', value: 'US'}], true),
+            FieldConfig.from('antal', 'Disponibla', 'input', null, false, true),
+            FieldConfig.from('fast', 'Fastställda', 'input', null, false, true),
+            FieldConfig.from('medd', 'Förklaring', 'input'),
+            FieldConfig.from('administration', 'Område', 'select', administrationOptions, true, true)
+          ];
+        });
+    } );
     // this.updateFieldsConfigByFetchingAdministrationsWithManagement();
   }
 
-  private updateFieldsConfigByFetchingWithADbJoin() {
-    this.http.get<Administration[]>('/api/administration')
-      .subscribe((administrations: Administration[]) => {
-        const administrationOptions = administrations.map(a => ({label: a.verks, value: a.id}));
-
-        this.fieldsConfigs = [
-          FieldConfig.from('avd', 'Namn', 'input', null, false, true),
-          FieldConfig.from('sjh', 'Sjukhus', 'select', [{label: 'NÄL', value: 'NÄL'}, {label: 'US', value: 'US'}], true),
-          FieldConfig.from('antal', 'Disponibla', 'input', null, false, true),
-          FieldConfig.from('fast', 'Fastställda', 'input', null, false, true),
-          FieldConfig.from('medd', 'Förklaring', 'input'),
-          FieldConfig.from('administration', 'Område', 'select', administrationOptions, true, true)
-        ];
-      });
-  }
 
   // A fancy way of using RxJs to do the same thing that could be done with a db join
   private updateFieldsConfigByFetchingAdministrationsWithManagement() {
